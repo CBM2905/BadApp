35da7702787871e719b0a25710b975af2e71c2c8
CBM <cbmillos29@gmail.com>
2025-04-09 14:36:56 -0500

    Ranking Implemented (the first four TOP) Globally

--- src/app/service/firebase.service.ts
+++ src/app/service/firebase.service.ts
@@ -1,5 +1,5 @@
 import { inject, Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, addDoc } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, addDoc, setDoc, doc, updateDoc, query, orderBy, limit} from '@angular/fire/firestore';
 import { Observable, BehaviorSubject } from 'rxjs';
 @Injectable({
   providedIn: 'root'
@@ -7,14 +7,28 @@ import { Observable, BehaviorSubject } from 'rxjs';
 export class FirebaseService {
   private firestore: Firestore = inject(Firestore);
   quizList$!: Observable<any[]>;
+  ranking$!: Observable<any[]>;
   
   constructor() {
     const quizCollection = collection(this.firestore, "Quiz");
+    const usersCollection = collection(this.firestore, "Users");
+    let queryRanking = query(usersCollection, orderBy('puntuacion', 'desc'), limit(4));
     this.quizList$ = collectionData(quizCollection);
+    this.ranking$ = collectionData(queryRanking);
   }
 
-  newDoc(data: {}){
-    addDoc(collection(this.firestore, "Quiz"), data);
+  newDoc(data: {}, collectionName: string){
+    addDoc(collection(this.firestore, collectionName), data);
+  }
+
+  
+  newDocSetId(data: {}, collectionName: string, id: string){
+    setDoc(doc(this.firestore, collectionName, id), data);
+  }
+
+  updatePuntuacion(id: any, data: {}){
+    let refDoc = doc(this.firestore, "Users", id);
+    updateDoc(refDoc, data);
   }
 
 
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { IonModal } from '@ionic/angular';
 import {ItemReorderEventDetail, OverlayEventDetail} from '@ionic/core/components';
+import { FirebaseService } from '../service/firebase.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: './tab2.page.html',
@@ -8,38 +9,19 @@ import {ItemReorderEventDetail, OverlayEventDetail} from '@ionic/core/components
   standalone: false,
 })
 export class Tab2Page implements OnInit {
-  name!: string;
-  date: any;
-  message = 'This modal example uses triggers to automatically open a modal when the button is clicked.';
-  @ViewChild(IonModal) modal!: IonModal;
-  constructor() { }
+ 
+  constructor(public firestore: FirebaseService) {
+    
+  }
 
   ngOnInit() {
   }
-  cancel() {
-    this.modal.dismiss(null, 'cancel');
-  }
 
-  confirm() {
-    this.modal.dismiss(this.name, 'confirm');
-  }
-
-  onWillDismiss(event: CustomEvent<OverlayEventDetail>) {
-    if (event.detail.role === 'confirm') {
-      this.message = `Hello, ${event.detail.data}!`;
-    }
-  }
-  
-  handleReorder(event: CustomEvent<ItemReorderEventDetail>) {
-    // The `from` and `to` properties contain the index of the item
-    // when the drag started and ended, respectively
-    console.log('Dragged from index', event.detail.from, 'to', event.detail.to);
-
-    // Finish the reorder and position the item in the DOM based on
-    // where the gesture ended. This method can also be called directly
-    // by the reorder group
-    event.detail.complete();
-  }
   
 
+  
+
+  
+
+  
 }
